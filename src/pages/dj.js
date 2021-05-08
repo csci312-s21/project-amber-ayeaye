01815f914f894f0c239b26f2ebaa48d09d4053a8
Replit user <replituser@example.com>
2021-05-08 20:35:08 +0000

    renamed one thing

--- src/pages/dj.js
+++ src/pages/dj.js
@@ -9,7 +9,7 @@ import sampleData from "../../data/songseed.json";
 import Grid from "@material-ui/core/Grid";
 import Link from "next/link";
 
-export default function Home() {
+export default function DJ() {
 
     const [currentSongs, setCurrentSongs] = useState(sampleData);
     const [addingMode, setAddingMode] = useState("search"); // other option is "manual"
