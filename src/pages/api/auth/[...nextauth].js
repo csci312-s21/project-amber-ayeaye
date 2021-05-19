d80cd96dde377bcff263136dd540c3719eb4f1da
Assadou  Nzigamasabo <>
2021-05-19 04:05:38 +0000

    updated database.sqlite3 file

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -10,7 +10,7 @@ const options = {
     Providers.Auth0({
       clientId: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
-      domain: process.env.AUTH0_DOMAIN
+      domain: process.env.AUTH0_DOMAIN,
     })
   ],
   secret: process.env.NEXTAUTH_SECRET,
