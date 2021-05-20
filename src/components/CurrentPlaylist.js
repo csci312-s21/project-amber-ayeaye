6f72f2c6e010df44cc5d42b2331fb753d6ea8e54
Myles Stokowski <mylesstokowski@gmail.com>
2021-05-20 11:46:56 -0400

    get current playlist from server

--- src/components/CurrentPlaylist.js
+++ src/components/CurrentPlaylist.js
@@ -1,21 +1,46 @@
 import Playlist from "./Playlist";
 import { useEffect, useState } from "react";
 
-export default function CurrentPlaylist({ playlistId }) {
+export default function CurrentPlaylist() {
   const [playlistSongs, setPlaylistSongs] = useState();
+  const [currentPlaylistId, setCurrentPlaylistId] = useState();
 
-  useEffect(() => {
+  useEffect(async () => {
+    const getId = async () => {
+      const response = await fetch("api/currentplaylist");
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      const id = await response.json();
+      setCurrentPlaylistId(id);
+    };
+    await getId();
+
+    if (currentPlaylistId) {
+      console.log(currentPlaylistId);
+    } else {
+      console.log("no current playlist");
+    }
+
     setInterval(() => {
       const getPlaylistSongs = async () => {
-        const response = await fetch(`api/playlistsongs/${playlistId}`);
+        const response = await fetch(`api/playlistsongs/${currentPlaylistId}`);
         if (!response.ok) {
           throw new Error(response.statusText);
         }
         const data = await response.json();
-        setPlaylistSongs(data);
+        currentPlaylistId && setPlaylistSongs(data);
       };
-      getPlaylistSongs();
+      if (currentPlaylistId) {
+        getPlaylistSongs();
+        console.log(playlistSongs);
+      }
     }, 3000);
   }, []);
-  return <Playlist songs={playlistSongs} mode={"inPlaylist"} />;
+
+  return currentPlaylistId && playlistSongs ? (
+    <Playlist songs={playlistSongs} mode={"inPlaylist"} />
+  ) : (
+    <div> no current playlist </div>
+  );
 }
