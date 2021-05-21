c31cbe22c05d5f5b4c2c1525ffb2b4c55b1fc7ab
Assadou  Nzigamasabo <>
2021-05-21 19:43:59 +0000

    added new playButton

--- src/components/PlayButton.js
+++ src/components/PlayButton.js
@@ -1,26 +1,15 @@
-// import { useState } from "react";
-import { FaPlayCircle, FaPauseCircle } from "react-icons/fa";
-import Button from "@material-ui/core/Button";
-
-export default function PlayButton({ isPlaying, playOrPause }) {
-  // const url = "http://boombox.middlebury.edu:8000/";
-  // // const audio = new Audio(url);
-
-  // const playAudio = () => {
-  //   audio.play()
-  // }
-
+export default function PlayButton() {
   return (
     <div>
-      {isPlaying ? (
-        <Button onClick={() => playOrPause()}>
-          <FaPauseCircle size={30} />
-        </Button>
-      ) : (
-        <Button onClick={() => playOrPause()}>
-          <FaPlayCircle size={30} />
-        </Button>
-      )}
+      <iframe 
+        title= "Listen to WRMC live"
+        src="https://onlineradiobox.com/us/wrmc/?cs=us.wrmc" 
+        width="300" 
+        height="155" 
+        frameborder="0" 
+        scrolling="no" 
+        style={{padding: "10px 20px"}}>
+      </iframe>
     </div>
   );
 }
