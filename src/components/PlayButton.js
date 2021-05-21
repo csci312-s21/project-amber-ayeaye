1d338e333f3438d0986018dac782331bc28d4c2a
Assadou  Nzigamasabo <>
2021-05-21 19:49:16 +0000

    lint fix

--- src/components/PlayButton.js
+++ src/components/PlayButton.js
@@ -6,10 +6,9 @@ export default function PlayButton() {
         src="https://onlineradiobox.com/us/wrmc/?cs=us.wrmc" 
         width="300" 
         height="155" 
-        frameborder="0" 
+        frameBorder="0" 
         scrolling="no" 
-        style={{padding: "10px 20px"}}>
-      </iframe>
+        style={{padding: "10px 20px"}} />
     </div>
   );
 }
