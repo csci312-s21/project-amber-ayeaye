e8e17d00c8d9bd39d17245b57971012c13419fdc
Ntare62 <58796154+Ntare62@users.noreply.github.com>
2021-05-22 10:28:03 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import styles from "../styles/Home.module.css";
 import Head from "next/head";
 import LoginWidget from "../components/LoginWidget";
 import PlayButton from "../components/PlayButton";
-import { useState, useEffect } from "react";
+//import { useState, useEffect } from "react";
 import Link from "next/link";
 import PlaylistExplorer from "../components/PlaylistExplorer";
 import Button from "@material-ui/core/Button";
@@ -10,64 +10,34 @@ import Schedule from "../components/Schedule";
 import { useSession } from "next-auth/client";
 
 export default function Home() {
-  const [showSecret, setShowSecret] = useState();
-  const [session] = useSession();
+    const [session] = useSession();
 
-  session && console.log(session.user.email);
-  useEffect(() => {
-    const getUser = async () => {
-      if (session) {
-        const response = await fetch("/api/secret");
-        if (response.ok) {
-          const username = await response.json();
-          setShowSecret(username);
-          console.log(username);
-        } else {
-          setShowSecret(null);
-        }
-      } else {
-        setShowSecret(null);
-      }
-    };
-    getUser();
-  }, [session]);
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>WRMC</title>
-        <link rel="icon" href="/favicon.ico" />
+    return ( 
+    <div className = { styles.container } >
+        <Head>
+            <title > WRMC </title> 
 
-        <img
-          src="https://wrmc.middlebury.edu/wp-content/themes/wrmc/images/logo_large.png"
-          width="400"
-          alt="WRMC 91.1 FM Middlebury College Radio 91.1 FM"
-        />
-      </Head>
+            <link rel = "icon"
+            href = "/favicon.ico"/>
 
-      <main>
-        <h1>Welcome to WRMC!</h1>
-        <LoginWidget />
-        <Schedule />
-        <PlaylistExplorer />
+            <img src = "https://wrmc.middlebury.edu/wp-content/themes/wrmc/images/logo_large.png"
+            width = "400"
+            alt = "WRMC 91.1 FM Middlebury College Radio 91.1 FM"/>
+        </Head>
 
-        {showSecret && (
-          <Link href="/dj">
-            <Button variant="contained" color="secondary">
-              DJ Page
-            </Button>
-          </Link>
-        )}
-
-        <Link href="/dj">
-          <Button variant="contained" color="secondary">
-            DJ Page
-          </Button>
-        </Link>
-
-        <PlayButton />
-      </main>
-      <footer>A CS 312 Project</footer>
+        <main>
+            <h1> Welcome to WRMC! </h1> 
+            {session &&
+            <Link href = "/dj">
+            <Button variant = "contained" color = "secondary">DJ Page</Button> 
+            </Link>}
+            <LoginWidget />
+            <Schedule />
+            <PlaylistExplorer />
+            <PlayButton />
+        </main> 
+        <footer> A CS 312 Project </footer> 
     </div>
-  );
+);
 }
