91f04ed921686167ab5db545c0f3217309c34771
Simon Scharf <simonmscharf@gmail.com>
2021-05-22 11:18:59 -0400

    fixed positioning of login button

--- src/components/LoginWidget.js
+++ src/components/LoginWidget.js
@@ -1,36 +1,49 @@
 import Button from "@material-ui/core/Button";
 //import { positions } from "@material-ui/system";
-import {
-  signIn, 
-  signOut,
-  useSession
-} from "next-auth/client";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  login: {
+    color: "white",
+    backgroundColor: "blue",
+    position: "absolute",
+    top: 20,
+    right: 20,
+  },
+}));
+
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export default function LoginWidget() {
-  const [ session ] = useSession()
+  const [session] = useSession();
+  const classes = useStyles();
 
-
-  if (session){
-return (<div>
+  if (session) {
+    return (
+      <div>
         <p>
-          Signed in as {session.user.name} 
+          Signed in as {session.user.name}
           <Button
-            variant="contained" 
+            variant="contained"
             color="primary"
             size="small"
-           onClick={signOut}>Sign out
-          </Button> 
+            onClick={signOut}
+          >
+            Sign out
+          </Button>
         </p>
-         </div>);
-  }else{
-    
-    return (<div>
-            <Button
-              variant="contained" 
-              color="primary"
-             onClick={signIn}>Login as a Dj
-            </Button>
-         </div>);
-
+      </div>
+    );
+  } else {
+    return (
+      <Button
+        className={classes.login}
+        variant="contained"
+        color="primary"
+        onClick={signIn}
+      >
+        Login as a Dj
+      </Button>
+    );
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,87 +1,95 @@
-
 import styles from "../styles/Home.module.css";
 import Head from "next/head";
 import LoginWidget from "../components/LoginWidget";
 import PlayButton from "../components/PlayButton";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import PlaylistExplorer from "../components/PlaylistExplorer";
-import Button from "@material-ui/core/Button";
 import Schedule from "../components/Schedule";
-import {useSession} from "next-auth/client";
+import { useSession } from "next-auth/client";
+
+import { Grid, Button } from "@material-ui/core/";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  header: {
+    padding: 10,
+  },
+}));
 
 export default function Home() {
+  const classes = useStyles();
 
-const [showSecret, setShowSecret] = useState();
-const [session] = useSession();
+  const [showSecret, setShowSecret] = useState();
+  const [session] = useSession();
 
-session && console.log(session.user.email)
-  useEffect(()=>{
-    const getUser = async ()=>{
-      if (session){
+  session && console.log(session.user.email);
+  useEffect(() => {
+    const getUser = async () => {
+      if (session) {
         const response = await fetch("/api/secret");
-        if (response.ok){
-
+        if (response.ok) {
           const username = await response.json();
           setShowSecret(username);
-          console.log(username)
+          console.log(username);
+        } else {
+          setShowSecret(null);
         }
-        else{
+      } else {
         setShowSecret(null);
-        }
-     }
-     else{
-       setShowSecret(null);
-     }
+      }
     };
     getUser();
   }, [session]);
 
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>WRMC</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
+      <main>
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          spacing={5}
+          className={classes.header}
+        >
+          <Grid container justify="center" spacing={8} direction="row">
+            <Grid item xs={12}>
+              <img
+                className="logo"
+                src="https://wrmc.middlebury.edu/wp-content/themes/wrmc/images/logo_large.png"
+                width="400"
+                alt="WRMC 91.1 FM Middlebury College Radio 91.1 FM"
+              />
+            </Grid>
+          </Grid>
+          <LoginWidget className={classes.login} />
 
-    return (
-        <div className={styles.container}>
+          <Link href="/dj">
+            <Button variant="contained" color="secondary">
+              DJ Page
+            </Button>
+          </Link>
 
-        <Head>
-            <title>WRMC</title>
-            <link rel="icon" href="/favicon.ico" />
+          <Schedule />
+          <PlaylistExplorer />
 
-            <img src="https://wrmc.middlebury.edu/wp-content/themes/wrmc/images/logo_large.png" width="400"  alt="WRMC 91.1 FM Middlebury College Radio 91.1 FM"/>
-        </Head>
-
-        <main>
-            <h1>
-
-            Welcome to WRMC!
-            </h1>
-            <LoginWidget/>
-            <Schedule/>
-            <PlaylistExplorer/>
-
-            {showSecret && <Link href="/dj">
-              <Button
-                variant="contained"
-                color="secondary"
-
-                >DJ Page
-              </Button>
-            </Link>}
-
+          {showSecret && (
             <Link href="/dj">
-              <Button
-                variant="contained"
-                color="secondary"
-
-                >DJ Page
+              <Button variant="contained" color="secondary">
+                DJ Page
               </Button>
             </Link>
+          )}
+        </Grid>
 
-            
-
-        <PlayButton/>
-
-        </main>
-        <footer>A CS 312 Project</footer>
-        </div>
-    );
+        <PlayButton />
+      </main>
+      <footer>A CS 312 Project</footer>
+    </div>
+  );
 }
