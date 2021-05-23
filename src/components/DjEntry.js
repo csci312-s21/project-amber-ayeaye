a5f8469312447e31675e34cf4d1046c04bdfa403
Replit user <replituser@example.com>
2021-05-23 15:49:23 +0000

    first draft of redesign

--- src/components/DjEntry.js
+++ src/components/DjEntry.js
@@ -8,9 +8,12 @@ import {
     Button,
     Container
 } from "@material-ui/core";
-
 import AddIcon from "@material-ui/icons/Add";
 import CancelIcon from "@material-ui/icons/Cancel";
+import styles from "../styles/Home.module.css";
+import Head from "next/head";
+import LoginWidget from "../components/LoginWidget";
+import { makeStyles } from "@material-ui/core/styles";
 
 export default function DjEntry() {
     
@@ -21,6 +24,22 @@ export default function DjEntry() {
     const [currentShowId, setCurrentShowId] = useState();
     const [songPlayOrder, setSongPlayOrder] = useState();
     const [lastCurrentPlaylistId, setLastCurrentPlaylistId] = useState();
+
+    const useStyles = makeStyles(() => ({
+      header: {
+        padding: 20,
+        backgroundColor: "#90d7ed",
+        width: "100%",
+      },
+      body: {
+        padding: 60,
+      },
+      logo: {
+        margin: "auto",
+      },
+    }));
+
+    const classes = useStyles();
 
     // Set the current show based on an ID
     const setCurrentShow = id => setCurrentShowId(id);
@@ -224,71 +243,163 @@ export default function DjEntry() {
   
     }
 
-    return (
-        <Container>
-            <Grid
-                container
-                spacing={6}
-                direction="row"
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>WRMC</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={classes.header}>
+        <Grid container justify="center">
+          <img
+            className="logo"
+            src="https://wrmc.middlebury.edu/wp-content/themes/wrmc/images/logo_large.png"
+            width="400"
+            alt="WRMC 91.1 FM Middlebury College Radio 91.1 FM"
+          />
+        </Grid>
+          <Link href="/">
+            <Button
+              variant="contained"
+              color="secondary"
+              style={{ position: "absolute", top: 20, right: 20 }}
             >
-                
-                <Grid item xs={6}>
-                    <CurrentShowSetter setCurrentShow={setCurrentShow}/>
-                </Grid>
-        
-                <Grid item xs={6}>
-                    { !editingPlaylistId ?
-                        <Button
-                            id="newPlaylistButton"
-                            variant="contained"
-                            color="primary"
-                            size="medium"
-                            disabled={!currentShowId}
-                            startIcon={<AddIcon />}
-                            onClick={() => createNewPlaylist(currentShowId)}
-                        >
-                            New Playlist
-                        </Button>
-                            :
-                        <Button
-                            id="cancelButton"
-                            variant="contained"
-                            color="primary"
-                            size="medium"
-                            startIcon={<CancelIcon />}
-                            onClick={() => deletePlaylist(editingPlaylistId)}
-                            >
-                            Cancel
-                        </Button>
-                    }
-                </Grid>
+              Home
+            </Button>
+          </Link>
+      </div>
 
-            </Grid>
-
+      <main>
+        <Grid
+          container
+          direction="column"
+          alignItems="center"
+          spacing={5}
+          justify="center"
+        >
+          <div className={classes.body}>
             <Grid
-                container
-                spacing={3}
+              container
+              direction="row"
+              alignItems="flex-start"
+              justify="center"
+              spacing={4}
             >
-
+              <Grid item>
+                <CurrentShowSetter setCurrentShow={setCurrentShow}/>
+              </Grid>
+              <Grid item>
+                { !editingPlaylistId ?
+                  <Button
+                      id="newPlaylistButton"
+                      variant="contained"
+                      color="primary"
+                      size="medium"
+                      disabled={!currentShowId}
+                      startIcon={<AddIcon />}
+                      onClick={() => createNewPlaylist(currentShowId)}
+                  >
+                      New Playlist
+                  </Button>
+                      :
+                  <Button
+                      id="cancelButton"
+                      variant="contained"
+                      color="primary"
+                      size="medium"
+                      startIcon={<CancelIcon />}
+                      onClick={() => deletePlaylist(editingPlaylistId)}
+                      >
+                      Cancel
+                  </Button>
+                }
+              </Grid>
+              <Grid item>
                 {editingPlaylistId && <Grid 
-                    item xs={6}>
-                    {addingMode==="search" ? 
-                        <SearchBar addSongToPlaylist={addSongToPlaylist} switchMode={switchMode}/> :
-                        <ManualEntry addSongToPlaylist={addSongToPlaylist} switchMode={switchMode}/>
-                    }
+                  item xs={6}>
+                  {addingMode==="search" ? 
+                      <SearchBar addSongToPlaylist={addSongToPlaylist} switchMode={switchMode}/> :
+                      <ManualEntry addSongToPlaylist={addSongToPlaylist} switchMode={switchMode}/>
+                  }
                 </Grid>}
-
-                <Grid item xs={6}>
-                    {editingPlaylistSongs && <Playlist 
-                        songs={editingPlaylistSongs} 
-                        deleteSong={deleteSongFromPlaylist}
-                        mode={"inPlaylist"}/>}
-                </Grid>
-            
+              <Grid item>
+                <CurrentPlaylist />
+              </Grid>
             </Grid>
-        
-        </Container>
+            </Grid>
+          </div>
+        </Grid>
+      </main>
+      <footer>A CS 312 Project</footer>
+    </div>
+  );
 
-    )
+    // return (
+    //     <Container>
+    //         <Grid
+    //             container
+    //             spacing={6}
+    //             direction="row"
+    //         >
+                
+    //             <Grid item xs={6}>
+    //                 <CurrentShowSetter setCurrentShow={setCurrentShow}/>
+    //             </Grid>
+        
+    //             <Grid item xs={6}>
+    //                 { !editingPlaylistId ?
+    //                     <Button
+    //                         id="newPlaylistButton"
+    //                         variant="contained"
+    //                         color="primary"
+    //                         size="medium"
+    //                         disabled={!currentShowId}
+    //                         startIcon={<AddIcon />}
+    //                         onClick={() => createNewPlaylist(currentShowId)}
+    //                     >
+    //                         New Playlist
+    //                     </Button>
+    //                         :
+    //                     <Button
+    //                         id="cancelButton"
+    //                         variant="contained"
+    //                         color="primary"
+    //                         size="medium"
+    //                         startIcon={<CancelIcon />}
+    //                         onClick={() => deletePlaylist(editingPlaylistId)}
+    //                         >
+    //                         Cancel
+    //                     </Button>
+    //                 }
+    //             </Grid>
+
+    //         </Grid>
+
+    //         <Grid
+    //             container
+    //             spacing={3}
+    //         >
+
+    //             {editingPlaylistId && <Grid 
+    //                 item xs={6}>
+    //                 {addingMode==="search" ? 
+    //                     <SearchBar addSongToPlaylist={addSongToPlaylist} switchMode={switchMode}/> :
+    //                     <ManualEntry addSongToPlaylist={addSongToPlaylist} switchMode={switchMode}/>
+    //                 }
+    //             </Grid>}
+
+    //             <Grid item xs={6}>
+    //                 {editingPlaylistSongs && <Playlist 
+    //                     songs={editingPlaylistSongs} 
+    //                     deleteSong={deleteSongFromPlaylist}
+    //                     mode={"inPlaylist"}/>}
+    //             </Grid>
+            
+    //         </Grid>
+        
+    //     </Container>
+
+    // )
 
 }
