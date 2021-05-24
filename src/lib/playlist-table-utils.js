20d4660e5349d26a06f85056138f71c4273d11eb
Replit user <replituser@example.com>
2021-05-24 22:48:21 +0000

    fix

--- src/lib/playlist-table-utils.js
+++ src/lib/playlist-table-utils.js
@@ -51,7 +51,8 @@ export async function getPlaylist(id) {
  * @returns a Boolean indicating success
  */
 export async function deletePlaylist(id) {
-  const numDeleted = await knex("Playlist").where({ id: id }).del();
+  real_id = +id;
+  const numDeleted = await knex("Playlist").where({ id: real_id }).del();
   return numDeleted ? true : false;
 }
 
